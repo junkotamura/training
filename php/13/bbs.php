b12c4958ca821a99e953d82eea459302d0b045f8
junkotamura <junko.tamura@talkoot.co.jp>
2018-05-28 19:01:31 +0900

    終えたところまでアップします。

--- php/13/bbs.php
+++ php/13/bbs.php
@@ -1,64 +1,97 @@
 <?php
-function h($s) {
-  return htmlspecialchars($s, ENT_QUOTES, "UTF-8");
-}
+$filename = './review.txt';
+$name = 0;
+$comment = 0;
+$name_max = 20;
+$comment_max = 100;
 date_default_timezone_set('Asia/Tokyo');
-$err_msg1 = "";
-$err_msg2 = "";
-$err_msg3 = "";
-$err_msg4 = "";
-$name = (isset($_POST["comment"]) === true) ?$_POST["name"]: "";
-$comment = (isset($_POST["comment"]) === true) ? trim($_POST["comment"]): "";
+$log = date('Y-m-d H:i:s');
 
-if (  isset($_POST["submit"] ) ===  true ) {
-    if ( $name   === "" ) $err_msg1 = "名前を入力してください"; 
- 
-    if ( $comment  === "" )  $err_msg2 = "ひとことを入力してください";
- 
-    if( $err_msg1 === "" && $err_msg2 ==="" ){
-        $fp = fopen( "review.txt" ,"a" );
-        fwrite( $fp ,  $name."\t".$comment."\n");
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  $name = $_POST['name'];
+  $comment = $_POST['comment'];
+
+  if (($fp_name = fopen($filename , 'a')) !== FALSE) {
+    if (((mb_strlen($name) <= $name_max) && (mb_strlen($name) !== 0)) && ((mb_strlen($comment) <= $comment_max) && (mb_strlen($comment) !== 0))) {
+      if (fwrite($fp_name, "$name:") === FALSE) {
+          print 'ファイル書き込み失敗' . $filename;
+      }
     }
- 
+    fclose($fp_name);
+  }
+
+  $text = "$comment $log\n";
+  if (($fp_comment = fopen($filename, 'a')) !== FALSE) {
+    if (((mb_strlen($comment) <= $comment_max) && (mb_strlen($comment) !== 0)) && ((mb_strlen($name) <= $name_max) && (mb_strlen($name) !== 0))) {
+      if (fwrite($fp_comment, "$text") === FALSE) {
+          print 'ファイル書き込み失敗' . $filename;
+      }
+    }
+    fclose($fp_comment);
+  } 
 }
 
-$fp = fopen("review.txt","r");
- 
-$dataArr = array();
-while($res = fgets($fp)){
-    $tmp = explode("\t",$res);
-    $arr = array(
-        "name"=>$tmp[0],
-        "comment"=>$tmp[1]
-    );
-    $dataArr[]= $arr;
-} 
+$data = array();
+
+  if (is_readable($filename) === TRUE) {
+      if (($fp = fopen($filename,'r')) !== FALSE) {
+           while(($tmp = fgets($fp)) !== FALSE) {
+           $data[] = htmlspecialchars($tmp, ENT_QUOTES, 'UTF-8');
+           }
+           fclose($fp);
+      }
+  } else {
+   $data[] = 'ファイルがありません';
+  }
 ?>
 
 <!DOCTYPE html>
 <html lamg="ja">
 <head>
   <meta charset="UTF-8">
-  <title>bbs</title>
+  <title>ひとこと掲示板</title>
 </head>
 <body>
   <h1>ひとこと掲示板</h1>
-  <ul>
-  <li><?php echo $err_msg1; ?></li>
-  <li><?php echo $err_msg2; ?></li>
-  </ul>
+  <?php if (mb_strlen($name) > $name_max) { ?>
+    <ul>
+      <li>
+  <?php print '名前は20文字以内で入力してください'; ?>
+      </li>
+    </ul>
+  <?php } ?>
+  <?php if (mb_strlen($name) === 0) { ?>
+    <ul>
+      <li>
+  <?php print '名前を入力してください'; ?>
+      </li>
+    </ul>
+  <?php } ?>
+  <?php if (mb_strlen($comment) > $comment_max) { ?>
+    <ul>
+      <li>
+  <?php print 'ひとことは100文字以内で入力してください'; ?>
+      </li>
+    </ul>
+  <?php } ?>
+  <?php if (mb_strlen($comment) === 0) { ?>
+    <ul>
+      <li>
+  <?php print 'ひとことを入力してください'; ?>
+      </li>
+    </ul>
+  <?php } ?>
   <form method="post">
-    名前：<input type="text" name="name" value="<?php echo $name; ?>">
-    ひとこと：<input type="text" name="comment" value="<?php echo $comment; ?>">
-
+    名前：<input type="text" name="name" value="">
+    ひとこと：<input type="text" name="comment" size= "50" value="">
     <input type="submit" name="submit" value="送信">
   </form>
-  <ul>
-    <?php foreach ($dataArr as $data): ?>
-        <li>
-          <span><?php echo $data["name"]; ?></span>：<span><?php echo $data["comment"]; ?></span>
-        </li>
-    <?php endforeach; ?>
-　</ul>
+  <?php foreach ($data as $read) { ?>
+    <ul>
+      <li>
+  <?php print $read; ?>
+      </li>
+  　</ul>
+  <?php } ?>
 </body>
 </html>
